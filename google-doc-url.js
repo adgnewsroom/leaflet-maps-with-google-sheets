1a9548a06a1ba4838a3587207fe2556e639c81f1
Arkansas Democrat-Gazette <59581933+adgnewsroom@users.noreply.github.com>
2020-03-19 21:34:27 -0500

    update google sheets link

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1s-8O54qanmeEfgBhPYZQ9v_1SoHOYa325zy9JFCOLkI/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1FqicusE4OHptozPdesxCwAX4eXo65nsGOZ-XSga85Gc/edit#gid=0';
